1c5a002bdd2dd24508e531ce134b268e0fd62cee
Nathan LEBON <nathan.lebon@epitech.eu>
2020-06-20 17:42:39 +0200

    Starting tutorial 5

---
+++ Assets/Controllers/MouseController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour
+{
+    public GameObject circleCursor;
+    Vector3 _lastFrameCameraPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 currFrameCameraPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        currFrameCameraPos.z = 0;
+
+        //update Circle Cursor pos
+        circleCursor.transform.position = currFrameCameraPos;
+
+        //Handle screen drag
+        if (Input.GetMouseButton(1) || Input.GetMouseButton(2))
+        {
+            Vector3 diff = _lastFrameCameraPos - currFrameCameraPos;
+            Camera.main.transform.Translate(diff);
+        }
+        _lastFrameCameraPos = Camera.main.ScreenToWorldPoint(Input.mousePosition); ;
+        _lastFrameCameraPos.z = 0;
+    }
+}
