5c1789216ee97357136226c6c835339fdd5a14b1
Nathan LEBON <nathan.lebon@epitech.eu>
2020-06-26 00:34:16 +0200

    Starting Tutorial 7

--- Assets/Controllers/MouseController.cs
+++ Assets/Controllers/MouseController.cs
@@ -1,10 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class MouseController : MonoBehaviour
 {
-	public GameObject circleCursor;
+	public GameObject circleCursorPrefab;
 
 	// The world-position of the mouse last frame.
 	Vector3 lastFramePosition;
@@ -12,11 +11,11 @@ public class MouseController : MonoBehaviour
 
 	// The world-position start of our left-mouse drag operation
 	Vector3 dragStartPosition;
-
+	List<GameObject> _selectedTilesHintGameObject;
 	// Use this for initialization
 	void Start()
 	{
-
+		_selectedTilesHintGameObject = new List<GameObject>();
 	}
 
 	// Update is called once per frame
@@ -25,7 +24,7 @@ public class MouseController : MonoBehaviour
 		currFramePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 		currFramePosition.z = 0;
 
-		UpdateCursor();
+		//UpdateCursor();
 		UpdateDragging();
 		UpdateCameraMovement();
 
@@ -35,53 +34,82 @@ public class MouseController : MonoBehaviour
 		lastFramePosition.z = 0;
 	}
 
-	void UpdateCursor()
-	{
-		// Update the circle cursor position
-		Tile tileUnderMouse = MapController.Instance.GetTileAtWorldCoord(currFramePosition);
-		if (tileUnderMouse != null)
-		{
-			circleCursor.SetActive(true);
-			Vector3 cursorPosition = new Vector3(tileUnderMouse.Position.x, tileUnderMouse.Position.y, 0);
-			circleCursor.transform.position = cursorPosition;
-		}
-		else
-		{
-			// Mouse is outside of the valid tile space, so hide the cursor.
-			circleCursor.SetActive(false);
-		}
-	}
+	//void UpdateCursor()
+	//{
+	//	// Update the circle cursor position
+	//	Tile tileUnderMouse = MapController.Instance.GetTileAtWorldCoord(currFramePosition);
+	//	if (tileUnderMouse != null)
+	//	{
+	//		circleCursor.SetActive(true);
+	//		Vector3 cursorPosition = new Vector3(tileUnderMouse.Position.x, tileUnderMouse.Position.y, 0);
+	//		circleCursor.transform.position = cursorPosition;
+	//	}
+	//	else
+	//	{
+	//		// Mouse is outside of the valid tile space, so hide the cursor.
+	//		circleCursor.SetActive(false);
+	//	}
+	//}
 
 	void UpdateDragging()
 	{
+
 		// Start Drag
 		if (Input.GetMouseButtonDown(0))
 		{
 			dragStartPosition = currFramePosition;
 		}
 
+		int start_x = Mathf.FloorToInt(dragStartPosition.x);
+		int end_x = Mathf.FloorToInt(currFramePosition.x);
+		int start_y = Mathf.FloorToInt(dragStartPosition.y);
+		int end_y = Mathf.FloorToInt(currFramePosition.y);
+
+		// We may be dragging in the "wrong" direction, so flip things if needed.
+		if (end_x < start_x)
+		{
+			int tmp = end_x;
+			end_x = start_x;
+			start_x = tmp;
+		}
+		if (end_y < start_y)
+		{
+			int tmp = end_y;
+			end_y = start_y;
+			start_y = tmp;
+		}
+
+		
+
+		// clean old drag previews
+		while (_selectedTilesHintGameObject.Count > 0)
+        {
+			GameObject go = _selectedTilesHintGameObject[0];
+			_selectedTilesHintGameObject.RemoveAt(0);
+			SimplePool.Despawn(go);
+        }
+
+		if (Input.GetMouseButton(0))
+        {
+			for (int x = start_x; x <= end_x; x++)
+			{
+				for (int y = start_y; y <= end_y; y++)
+				{
+					Tile t = MapController.Instance.Map.GetTileAt(x, y);
+					if (t != null)
+					{
+						//display building hint on top of the tile
+						GameObject go = SimplePool.Spawn(circleCursorPrefab, new Vector3(x, y, 0), Quaternion.identity);
+						go.transform.SetParent(this.transform, true);
+						_selectedTilesHintGameObject.Add(go);
+					}
+				}
+			}
+		}
+
 		// End Drag
 		if (Input.GetMouseButtonUp(0))
 		{
-			int start_x = Mathf.FloorToInt(dragStartPosition.x);
-			int end_x = Mathf.FloorToInt(currFramePosition.x);
-			int start_y = Mathf.FloorToInt(dragStartPosition.y);
-			int end_y = Mathf.FloorToInt(currFramePosition.y);
-
-			// We may be dragging in the "wrong" direction, so flip things if needed.
-			if (end_x < start_x)
-			{
-				int tmp = end_x;
-				end_x = start_x;
-				start_x = tmp;
-			}
-			if (end_y < start_y)
-			{
-				int tmp = end_y;
-				end_y = start_y;
-				start_y = tmp;
-			}
-
 			// Loop through all the tiles
 			for (int x = start_x; x <= end_x; x++)
 			{
@@ -105,7 +133,9 @@ public class MouseController : MonoBehaviour
 
 			Vector3 diff = lastFramePosition - currFramePosition;
 			Camera.main.transform.Translate(diff);
-
 		}
+
+		Camera.main.orthographicSize -= Camera.main.orthographicSize * Input.GetAxis("Mouse ScrollWheel");
+		Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize, 4f, 30f);
 	}
 }
